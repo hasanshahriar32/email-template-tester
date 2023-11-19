b4d52c1a2dfe21f25eb2ad5e713b56c4c9ced82a
Mr. Hasan <hasanshahriar32@gmail.com>
2023-11-19 14:30:11 +0600

    Add Tailwind CSS styles and build script

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const noRouteFound = require("./middleware/noRouteFound");
 const globalErrorHandler = require("./middleware/globalErrorHandler");
 const routes = require("./routes/routes.all");
 const bodyParser = require("body-parser");
+const path = require("path");
+const postcssMiddleware = require("postcss-middleware");
+const tailwindcss = require("tailwindcss");
 
 //making app :
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Dynamically generate and serve Tailwind CSS styles
+app.use(
+  "/public/styles.css",
+  postcssMiddleware({
+    plugins: [tailwindcss(path.resolve(__dirname, "tailwind.config.js"))],
+    src: (req) => path.resolve(__dirname, "styles.css"),
+  })
+);
 
 // test server
 app.get("/", (req, res) => {
@@ -23,12 +35,36 @@ app.get("/", (req, res) => {
   files.forEach((file) => {
     Urls.push(`/template?type=${file}`);
   });
-
   const modifiedUrlsHtml = Urls.map((url) => {
-    return `<a href="${url}">${url.split("=")[1].split(".")[0]}</a>`;
+    return `
+    <a href="${url}" class="transition-transform transform hover:scale-105">
+      <div class="max-w-xs bg-gradient-to-r from-purple-700 to-blue-500 text-white rounded-lg overflow-hidden shadow-lg m-4">
+        <div class="p-6">
+          <h5 class="text-2xl mb-0">${url.split("=")[1].split(".")[0]}</h5>
+        </div>
+      </div>
+    </a>
+  `;
   });
 
-  const html = modifiedUrlsHtml.join("<br/>");
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/styles.css">
+  </head>
+  <body class="font-sans bg-gradient-to-r from-purple-700 to-blue-500 text-white min-h-screen flex items-center justify-center">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      ${modifiedUrlsHtml.join("")}
+    </div>
+  </body>
+  </html>
+`;
+
+  res.send(html);
+
   res.send(html);
 });
 
