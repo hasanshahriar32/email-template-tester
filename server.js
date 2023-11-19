992c08b7a92da2bc5f4ce13f1e77de26a363aa5d
Mr. Hasan <hasanshahriar32@gmail.com>
2023-11-20 02:59:47 +0600

    Add mongoose package and connect to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const { port } = require("./utils/config.Env");
 const app = require("./app");
+const connectDB = require("./utils/mongodbConnect");
+connectDB();
 
 app.listen(port, console.log(`port connected ${port}`));
