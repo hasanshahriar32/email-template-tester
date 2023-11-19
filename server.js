8d0caf5f591c494e11e9484c9560a204ca92c3c0
Mr. Hasan <hasanshahriar32@gmail.com>
2023-11-20 03:13:55 +0600

    Update server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { port } = require("./utils/config.Env");
 const app = require("./app");
 const connectDB = require("./utils/mongodbConnect");
-connectDB();
+// const connectionStatus = connectDB();
 
 app.listen(port, console.log(`port connected ${port}`));
